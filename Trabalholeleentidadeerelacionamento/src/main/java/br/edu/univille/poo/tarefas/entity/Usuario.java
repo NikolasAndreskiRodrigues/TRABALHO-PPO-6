3b0a744a3c907d51100437ee2da05ef3b3039edc
NikolasAndreskiRodrigues <128433982+NikolasAndreskiRodrigues@users.noreply.github.com>
2024-09-26 20:34:06 -0300

    Add files via upload

---
+++ Trabalholeleentidadeerelacionamento/src/main/java/br/edu/univille/poo/tarefas/entity/Usuario.java
@@ -0,0 +1,24 @@
+package br.edu.univille.poo.tarefas.entity;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idUsuario;
+    private String email;
+    private String senha;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "idPerfil")
+    private Perfil perfil;
+
+    @OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL)
+    private List<Playlist> playlists;
+    
+
+}
